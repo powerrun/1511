35a7dbd8078a2ec35537e1862697e242336452f2
Cando <33193003+powerrun@users.noreply.github.com>
2024-02-29 17:32:55 +1100

    Create print_grid.c

---
+++ wk3/lab3/print_grid.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+struct grid
+{
+    int x;
+    int y;
+};
+
+int main(void)
+{
+    struct grid mygrid;
+    int i = 5;
+    mygrid.x = i;
+    mygrid.y = i;
+
+    while (y < i)
+    {
+        printf("(%d, %d)", x, y);
+
+
+    }
+
+
+}
+
+
+
+    return 0;
+}
