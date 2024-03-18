829e29f59ffc97b8f653a4f377e515b4ebd89f1d
Cando <33193003+powerrun@users.noreply.github.com>
2024-03-18 13:22:45 +0000

    Compare different versions of  word_square.c and retain the final version

--- wk5/lab5/word_square.c
+++ wk5/lab5/word_square.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_SIZE 1024
+#define MAX_SIZE 1025
 
 int main(void) {
     char word[MAX_SIZE];
 
     printf("Input word: ");
     fgets(word, MAX_SIZE, stdin);
-    int len;
-    len = strlen(word)-1;
-    printf("\nWord square is:\n");
-    for (int i = 0; i < len; i++) {
+
+    printf("Word square is:\n");
+    for (int i = strlen(word); i > 1; i--) {
+
         printf("%s", word);
     }
-    
+
     return 0;
 }
 
+// bug code
 
 // #include <stdio.h>
 // #include <string.h>
